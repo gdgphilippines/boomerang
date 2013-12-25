d0873b0dce4f7d29263e5063fc53a7d8008bffa4
Jomar Tigcal <jomar@tigcal.com>
2013-12-25 21:51:13 +0800

    Updating name and id, updating api key, and hiding cover photo

--- js/config.js
+++ js/config.js
@@ -1,17 +1,16 @@
 boomerang.factory('Config', function () {
     return {
-        //modify these
-        'name'          : 'GDG Space Coast',
-        'id'            : '103959793061819610212',
-        'google_api'    : 'AIzaSyA9ALjr2iWvhf3Rsz9-bH0cEcDcrdkpuAg',
-        'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
-        'cover' : {
-            title : 'GWT.create',
-            subtitle : 'The largest event about GWT in 2013 with 2 days and more than 60 presentations.',
-            button : {
-                text : 'Register',
-                url : 'http://gwtcreate.com/'
-            }
-        }
+        'name'          : 'GDG Philippines',
+        'id'            : '105750470224587854845',
+        'google_api'    : 'AIzaSyDhJlz9bfEcfzcSrq4FfxQrF6rf7k2SVtM',
+//        'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
+//        'cover' : {
+//            title : 'GWT.create',
+//            subtitle : 'The largest event about GWT in 2013 with 2 days and more than 60 presentations.',
+//            button : {
+//                text : 'Register',
+//                url : 'http://gwtcreate.com/'
+//            }
+//        }
     };
 });
