d85494fa1e7afb577623d25251effe5639ba2d82
Jomar Tigcal <jomar@tigcal.com>
2013-12-31 19:50:41 +0800

    Fixing missing comma in the config file

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ boomerang.factory('Config', function () {
     return {
         'name'          : 'GDG Philippines',
         'id'            : '105750470224587854845',
-        'google_api'    : 'AIzaSyDhJlz9bfEcfzcSrq4FfxQrF6rf7k2SVtM'
+        'google_api'    : 'AIzaSyDhJlz9bfEcfzcSrq4FfxQrF6rf7k2SVtM',
 //        'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
         'cover' : {
 //            title : 'GWT.create',
