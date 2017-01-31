2e0993a2af59c783f82af2408d4389a1d7f5b565
juvarabrera <juvar.abrera2@gmail.com>
2017-01-31 13:02:30 +0800

    Fixed mvc js. Fixed tensorflow dev summit website

--- scripts/App.js
+++ scripts/App.js
@@ -1,11 +1,145 @@
 var App = {
 	xhr: null,
 	fl: true,
+	CURRENT_CONTROLLER: "",
+	CURRENT_ACTION: "",
+	Constant: {
+		DEFAULT_CONTROLLER: "about",
+		DEFAULT_ACTION: "index",
+		BYPASS_CONTROLLER: []
+	},
+	Controller: {
+		"about": {
+			construct: function(parameters) {
+
+			},
+			index: function(parameters) {
+
+				return App.View.render();
+			}
+		}, 
+		"be": {
+			construct: function(parameters) {
+
+			}, 
+			"a-sponsor": function(parameters) {
+
+				return App.View.render();
+			}, 
+			"a-volunteer": function(parameters) {
+
+				return App.View.render();
+			}
+		}, 
+		"community": {
+			construct: function(parameters) {
+
+			}, 
+			"guidelines": function(parameters) {
+
+				return App.View.render();
+			}
+		}, 
+		"events": {
+			construct: function(parameters) {
+
+			}, 
+			index: function(parameters) {
+
+				return App.View.render();
+			}, 
+			"firebase-live-viewing": function(parameters) {
+
+				return App.View.render();
+			},
+			"tensorflow-dev-summit": function(parameters) {
+				return App.View.render(function() {
+					if(parameters[0] == "register")
+						$(".view .columns[name=event-info]").hide();
+					else
+						$(".view .columns[name=register]").hide();
+				})
+			}
+		}, 
+		"team": {
+			construct: function(parameters) {
+
+			},
+			index: function(parameters) {
+
+				return App.View.render();
+			}
+		}
+	},
+	View: {
+		render: function(callback) {
+			$("ul.nav a").removeClass("selected");
+			$("ul.nav a[data-page='"+App.CURRENT_CONTROLLER+"']").addClass("selected");
+			$(".loading").animate({
+				"top": "120px"
+			}, 400);
+			$(".view").html("").attr("controller", App.CURRENT_CONTROLLER);
+			if(App.fl)
+				App.fl = false;
+			else
+				App.xhr.abort();
+			$("body").scrollTop(0);
+			App.xhr = $.ajax({
+				url: "views/"+App.CURRENT_CONTROLLER+"/"+App.CURRENT_ACTION+".html",
+				cache: true,
+				success: function(html) {
+					$(".view").html(html).css("padding-top", "48px");
+					$(".loading").animate({
+						"top": "-70px"
+					}, 400);
+					Input.ready();
+					Form.ready();
+					if(callback)
+						callback();
+				},
+				error: function(xhrtemp, ajaxOptions, thrownError) {
+					if(xhrtemp.status == 404) {
+						$(".view").html("").load("views/error.html", function() {
+							$(".loading").animate({
+								"top": "-70px"
+							}, 400);
+						}).css("padding", "120px 0px")
+					}
+				}
+			})
+		}
+	},
+	Route: {
+		call: function(dir) {
+		var directory = dir.substring(1).split("/");
+		directory[0] = (directory[0] == "" || directory[0] == undefined) ? App.Constant.DEFAULT_CONTROLLER : directory[0];
+		directory[1] = (directory[1] == "" || directory[1] == undefined) ? App.Constant.DEFAULT_ACTION : directory[1];
+
+		App.CURRENT_CONTROLLER = directory[0];
+		App.CURRENT_ACTION = directory[1];
+		if(App.Controller.hasOwnProperty(App.CURRENT_CONTROLLER)) {
+			if(App.Controller[App.CURRENT_CONTROLLER].hasOwnProperty(App.CURRENT_ACTION)) {
+				App.Controller[App.CURRENT_CONTROLLER][App.CURRENT_ACTION](directory.splice(2));
+				if(App.Controller[App.CURRENT_CONTROLLER].hasOwnProperty("construct"))
+					App.Controller[App.CURRENT_CONTROLLER].construct(directory.splice(2));
+			} else {
+				if(App.Constant.BYPASS_CONTROLLER.indexOf(App.CURRENT_CONTROLLER) == -1) {
+					App.CURRENT_CONTROLLER = "error"; App.CURRENT_ACTION = "index";
+				}
+				App.View.render();
+			}
+		} else {
+			App.CURRENT_CONTROLLER = "error"; App.CURRENT_ACTION = "index";
+			App.View.render();
+		}
+	}
+	},
 	start: function(page) {
-		if(page == "")
-			page = Config.DEFAULT_CONTROLLER;
-		this.loadController(page);
+		this.Route.call(page);
 		this.static();
+		$(document).on("click", ".slider a", function() {
+			App.slider("hide");
+		});
 	},
 	static: function() {
 		App.responsive();
@@ -24,8 +158,13 @@ var App = {
 		$(".menu").click(function() {
 			App.slider("show");
 		})
-		// Input.ready();
-		// Form.ready();
+		$(document).on("change", "[data-show-if-checked]", function() {
+			$(this).parent().next().hide();
+			if($(this).is(":checked"))
+				$(this).parent().next().show();
+		})
+		Input.ready();
+		Form.ready();
 	},
 	slider: function(action) {
 		if(action == "show") {
@@ -49,37 +188,5 @@ var App = {
 			$(".action-bar .nav").hide();
 			$(".action-bar .menu").show();
 		}
-	},
-	loadController: function(controller) {
-		$("ul.nav a").removeClass("selected");
-		$("ul.nav a[data-page='"+controller+"']").addClass("selected");
-		$(".loading").animate({
-			"top": "120px"
-		}, 400);
-		$(".view").html("").attr("controller", controller);
-		if(App.fl)
-			App.fl = false;
-		else
-			App.xhr.abort();
-		$("body").scrollTop(0);
-		App.xhr = $.ajax({
-			url: "views/"+controller+".html",
-			cache: true,
-			success: function(html) {
-				$(".view").html(html).css("padding-top", "48px");
-				$(".loading").animate({
-					"top": "-70px"
-				}, 400);
-			},
-			error: function(xhrtemp, ajaxOptions, thrownError) {
-				if(xhrtemp.status == 404) {
-					$(".view").html("").load("views/error.html", function() {
-						$(".loading").animate({
-							"top": "-70px"
-						}, 400);
-					}).css("padding", "120px 0px")
-				}
-			}
-		})
 	}
 }
--- scripts/Input.js
+++ scripts/Input.js
@@ -5,40 +5,45 @@ Input = {
         this.readyTextArea();
     },
     readyInput: function() {
-        $("label input[type=text], label input[type=password], label input[type=date], label input[type=time], label input[type=email], label input[type=number]").each(function() {
+        $(document).find("label input[type=text], label input[type=password], label input[type=date], label input[type=time], label input[type=email], label input[type=number]").each(function() {
             $val = $(this).val();
             $type = $(this).attr("type");
             if($val != "" || $type == "date" || $type == "time")
                 $(this).siblings("span:not(.validate-message)").addClass("top");
-            $(this).parents("label").append("<border></border>");
-        }).focus(function() {
+            $(this).parents("label").append("<border><border>");
+        });
+        $(document).on("focus", "label input[type=text], label input[type=password], label input[type=date], label input[type=time], label input[type=email], label input[type=number]", function() {
             $(this).siblings("span:not(.validate-message)").addClass("top");
-        }).focusout(function() {
+        });
+        $(document).on("focusout", "label input[type=text], label input[type=password], label input[type=date], label input[type=time], label input[type=email], label input[type=number]", function() {
             $val = $(this).val();
             $type = $(this).attr("type");
             if($val == "" && !($type == "date" || $type == "time"))
                 $(this).siblings("span:not(.validate-message)").removeClass("top");
-        })
+        });
     },
     readySelect: function() {
-        $("label select").each(function() {
+        $(document).find("label select").each(function() {
             $(this).siblings("span:not(.validate-message)").addClass("top");
-        })
+        });
     },
     readyTextArea: function() {
         autosize(document.querySelectorAll('textarea'));
-        $("label textarea").each(function() {
+        $(document).find("label textarea").each(function() {
             $val = $(this).val();
             if($val != "")
                 $(this).siblings("span:not(.validate-message)").addClass("top");
-        }).focus(function() {
+            $(this).parents("label").append("<border><border>");
+        });
+        $(document).on("focus", "label textarea", function() {
             $(this).siblings("span:not(.validate-message)").addClass("top");
-        }).focusout(function() {
+        });
+        $(document).on("focusout", "label textarea", function() {
             $val = $(this).val();
             $type = $(this).attr("type");
             if($val == "")
                 $(this).siblings("span:not(.validate-message)").removeClass("top");
-        })
+        });
     },
     showMessage: function($input, $message) {
         if($input.siblings("span.validate-message").length != 0)
