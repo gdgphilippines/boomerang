fbc1772c4bc5f01b32a7947ba3bc861766939f80
juvarabrera <juvar.abrera2@gmail.com>
2017-03-29 17:40:29 +0800

    Changed sw

--- public/sw-config.js
+++ public/sw-config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  maximumFileSizeToCacheInBytes: 10485760
+  maximumFileSizeToCacheInBytes: 10485760,
+  staticFileGlobs: [
+  	"admin/**/*.*",
+  	"assets/**/*.*",
+  	"googlephlights/**/*.*",
+  	"iotsummit2017/**/*.*",
+  	"mediakit/**/*.*",
+  	"next17/**/*.*",
+  	"tensorflowdevsummit/**/*.*",
+  	"views/**/*.*",
+  	"index.html",
+  	"favicon.ico"
+  ]
 };
