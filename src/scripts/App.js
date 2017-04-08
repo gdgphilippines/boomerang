ed67aded0e7808bf39614d7272e54a25e49653ed
juvarabrera <juvar.abrera2@gmail.com>
2017-04-08 16:09:37 +0800

    Added next 17 venue and sponsor. Edited codeblaze

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -37,9 +37,9 @@ $.fn.enableScroll = function() {
 				$(".action-bar").removeClass("shadow");
 			}
 		});
-		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register('./service-worker.js');
-		}
+		// if ('serviceWorker' in navigator) {
+		// 	navigator.serviceWorker.register('./service-worker.js');
+		// }
 	},
 	"location": function(controller, action, parameters) {
 		App.CONTROLLER = controller.charAt(0).toUpperCase() + controller.slice(1) + "Controller";
@@ -91,24 +91,61 @@ $.fn.enableScroll = function() {
 			type: "get",
 			crossOrigin: true,
 			cache: true,
-			url: "https://api.meetup.com/gdgphilippines/events?callback=?",
+			url: "https://hub.gdgx.io/api/v1/chapters/105750470224587854845/events/upcoming?callback=?",
 			dataType: 'jsonp',
 			data: { 'headers': { 'Accept': 'application/json;' }, 'timeout': 10000 },
 			success: function(events) {
 				console.log(events);
-				if(events.data.length > 0) {
-					var ne = events.data[0];
-					var datex = new Date(ne.time);
-					$("#upcoming").html('<h3>'+ne.name+'</h3>'+ne.description+
-						'<p><i class="mdi mdi-map-marker" style="margin-right: 12px"></i>'+ne.venue.city+', '+ne.venue.localized_country_name+'</p>'+
-						'<p><i class="mdi mdi-calendar" style="margin-right: 12px"></i>'+datex.toDateString()+', '+datex.toLocaleTimeString()+'</p>'+
-						'<p><i class="mdi mdi-account-multiple" style="margin-right: 12px"></i>'+ne.yes_rsvp_count+' attending members</p>'+
-						'<p class="right"><a class="button" href="'+ne.link+'" target="_blank"><i class="mdi mdi-ticket" style="margin-right: 12px"></i>Apply for a Ticket</a></p>');
-					if(events.data.length > 1) {
+				var sort = function (prop, arr) {
+				    prop = prop.split('.');
+				    var len = prop.length;
+
+				    arr.sort(function (a, b) {
+				        var i = 0;
+				        var key;
+
+				        while( i < len ) {
+				            key = prop[i];
+
+				            if(!a.hasOwnProperty(key)) return 1;
+				            if(!b.hasOwnProperty(key)) return -1;
+
+				            a = a[key];
+				            b = b[key];
+				            i++;
+				        }
+				        if (a < b) {
+				            return -1;
+				        } else if (a > b) {
+				            return 1;
+				        } else {
+				            return 0;
+				        }
+				    });
+				    return arr;
+				}
+				events.items = sort("start", events.items);
+				console.log(events);
+				if(events.items.length == 0)
+					$("#upcoming").html('<h3>Message</h3><p>No incoming events for now. Add, like, and follow our social media pages to get the latest updates for our upcoming events!</p>');
+				else {
+					var nextEvent = events.items[0];
+					var datex = new Date(nextEvent.start.substring(0,19));
+					var tmp = document.createElement("DIV");
+					tmp.innerHTML = nextEvent.about;
+					$("#upcoming").html('<h3>'+nextEvent.title+'</h3>'+
+						'<p>'+((nextEvent.hasOwnProperty("about") ? tmp.textContent.split(/\s+/).slice(0,20).join(" ")+"..." : ''))+'</p>'+
+										'<p><i class="mdi mdi-map-marker" style="margin-right: 12px"></i>'+nextEvent.location+'</p>'+
+										'<p><i class="mdi mdi-calendar" style="margin-right: 12px"></i>'+dateFormat(datex, "dddd, mmmm dS, yyyy, h:MM TT")+'</p>'+
+						'<p class="right"><a class="button" href="'+nextEvent.eventUrl+'" target="_blank"><i class="mdi mdi-ticket" style="margin-right: 12px"></i>Apply for a Ticket</a></p>');
+					if(events.items.length > 1) {
 						$("#other_events").show().append('<div class="columns"><div class="col-12"></div></div>');
 						var first = true;
-						for(var i = 1; i < events.data.length; i++) {
-							datex = new Date(events.data[i].time);
+						for(var i = 1; i < events.items.length; i++) {
+							console.log(event);
+							var event = events.items[i];
+							datex = new Date(event.start.substring(0,19));
+							tmp = event.about;
 							var nomargin = "";
 							if(first) {
 								nomargin = ' style="margin-top: 0px;"';
@@ -116,20 +153,60 @@ $.fn.enableScroll = function() {
 							}
 							$("#other_events").find(".col-12").append('<div class="card"'+nomargin+'>'+
 									'<div class="wrapper">'+
-										'<h3>'+events.data[i].name+'</h3>'+ ((events.data[i].hasOwnProperty("venue")) ? 
-										'<p><i class="mdi mdi-map-marker" style="margin-right: 12px"></i>'+events.data[i].venue.city+', '+events.data[i].venue.localized_country_name+'</p>' : '')+
-										'<p><i class="mdi mdi-calendar" style="margin-right: 12px"></i>'+datex.toDateString()+', '+datex.toLocaleTimeString()+'</p>'+
-										'<p><i class="mdi mdi-account-multiple" style="margin-right: 12px"></i>'+events.data[i].yes_rsvp_count+' attending members</p>'+
-										'<p class="right"><a class="button" href="'+events.data[i].link+'" target="_blank"><i class="mdi mdi-ticket" style="margin-right: 12px"></i>Apply for a Ticket</a></p>'+
+										'<h3>'+event.title+'</h3>'+ 
+										'<p><i class="mdi mdi-map-marker" style="margin-right: 12px"></i>'+event.location+'</p>' +
+										'<p><i class="mdi mdi-calendar" style="margin-right: 12px"></i>'+dateFormat(datex, "dddd, mmmm dS, yyyy, h:MM TT")+'</p>'+
+										'<p class="right"><a class="button" href="'+event.eventUrl+'" target="_blank"><i class="mdi mdi-ticket" style="margin-right: 12px"></i>Apply for a Ticket</a></p>'+
 									'</div>'+
 								'</div>');
 						}
 					}
-				} else {
-					$("#upcoming").html('<h3>Message</h3><p>No incoming events for now. Add, like, and follow our social media pages to get the latest updates for our upcoming events!</p>');	
 				}
 			}
 		});
+		// $.ajax({
+		// 	type: "get",
+		// 	crossOrigin: true,
+		// 	cache: true,
+		// 	url: "https://api.meetup.com/gdgphilippines/events?callback=?",
+		// 	dataType: 'jsonp',
+		// 	data: { 'headers': { 'Accept': 'application/json;' }, 'timeout': 10000 },
+		// 	success: function(events) {
+		// 		console.log(events);
+		// 		if(events.data.length > 0) {
+		// 			var ne = events.data[0];
+		// 			var datex = new Date(ne.time);
+		// 			$("#upcoming").html('<h3>'+ne.name+'</h3>'+ne.description+
+		// 				'<p><i class="mdi mdi-map-marker" style="margin-right: 12px"></i>'+ne.venue.city+', '+ne.venue.localized_country_name+'</p>'+
+		// 				'<p><i class="mdi mdi-calendar" style="margin-right: 12px"></i>'+datex.toDateString()+', '+datex.toLocaleTimeString()+'</p>'+
+		// 				'<p><i class="mdi mdi-account-multiple" style="margin-right: 12px"></i>'+ne.yes_rsvp_count+' attending members</p>'+
+		// 				'<p class="right"><a class="button" href="'+ne.link+'" target="_blank"><i class="mdi mdi-ticket" style="margin-right: 12px"></i>Apply for a Ticket</a></p>');
+		// 			if(events.data.length > 1) {
+		// 				$("#other_events").show().append('<div class="columns"><div class="col-12"></div></div>');
+		// 				var first = true;
+		// 				for(var i = 1; i < events.data.length; i++) {
+		// 					datex = new Date(events.data[i].time);
+		// 					var nomargin = "";
+		// 					if(first) {
+		// 						nomargin = ' style="margin-top: 0px;"';
+		// 						first = false;
+		// 					}
+		// 					$("#other_events").find(".col-12").append('<div class="card"'+nomargin+'>'+
+		// 							'<div class="wrapper">'+
+		// 								'<h3>'+events.data[i].name+'</h3>'+ ((events.data[i].hasOwnProperty("venue")) ? 
+		// 								'<p><i class="mdi mdi-map-marker" style="margin-right: 12px"></i>'+events.data[i].venue.city+', '+events.data[i].venue.localized_country_name+'</p>' : '')+
+		// 								'<p><i class="mdi mdi-calendar" style="margin-right: 12px"></i>'+datex.toDateString()+', '+datex.toLocaleTimeString()+'</p>'+
+		// 								'<p><i class="mdi mdi-account-multiple" style="margin-right: 12px"></i>'+events.data[i].yes_rsvp_count+' attending members</p>'+
+		// 								'<p class="right"><a class="button" href="'+events.data[i].link+'" target="_blank"><i class="mdi mdi-ticket" style="margin-right: 12px"></i>Apply for a Ticket</a></p>'+
+		// 							'</div>'+
+		// 						'</div>');
+		// 				}
+		// 			}
+		// 		} else {
+		// 			$("#upcoming").html('<h3>Message</h3><p>No incoming events for now. Add, like, and follow our social media pages to get the latest updates for our upcoming events!</p>');	
+		// 		}
+		// 	}
+		// });
 	},
 	getBlogPosts: function(c) {
 		$.ajax({
